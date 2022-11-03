9679ba3feec49829dacef92a2f2f51bfc636d5d0
marijedb <marijedb@live.nl>
2022-11-03 21:11:21 +0100

    fully functional app

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
 import Dog from "./Dog.js";
 import { dogs } from "./data.js";
 
+let suitor = getNewDog()
+const likeBtn = document.getElementById("btn-like");
+const dislikeBtn = document.getElementById("btn-dislike");
+const likeIcon = document.getElementById("like-icon");
+const dislikeIcon = document.getElementById("dislike-icon");
+
+dislikeBtn.addEventListener("click", function(){
+    dislikeIcon.classList.remove("hidden")
+    disabledButton()
+    setTimeout(function(){
+        suitor = getNewDog();
+        dislikeIcon.classList.add("hidden")
+        disabledButton();
+        render();
+    },1500);
+})
+
+likeBtn.addEventListener("click", function(){
+    likeIcon.classList.remove("hidden");
+    disabledButton()
+    setTimeout(function(){
+        suitor = getNewDog();
+        likeIcon.classList.add("hidden")
+        disabledButton();
+        render();
+    },1500);
+})
+
+function disabledButton(){
+    if(likeBtn.disabled){
+        likeBtn.disabled = false;
+        dislikeBtn.disabled = false;
+    } else {
+        likeBtn.disabled = true;
+        dislikeBtn.disabled = true;
+    }
+}
+
 function getNewDog(){
     const newDog = dogs.shift();
     return newDog ? new Dog(newDog) : {};
 }
 
-let suitor = getNewDog()
-
 function render(){
-    document.getElementById("dog-data").innerHTML = suitor.getDogHtml();
+    if(suitor.getDogHtml){
+        document.getElementById("dog-data").innerHTML = suitor.getDogHtml();
+    } else {
+        disabledButton();
+        document.getElementById("dog-data").innerHTML = `
+        <div class="end-message">
+            <h1 class="end-title">Looks like we are out of dogs for you to play around with!</h1>
+            <p>Please come back later...</p>
+        </div>`;
+    }
 }
 
 render()
-// GET DOG PIC
-// GET DOG DESCRIPTION
-// GET NEW ONE WITH LIKE OR DISLIKE
-// END OF PICTURES DATA COME BACK LATER TEXT
